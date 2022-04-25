457933e2dddb8d0dd838264e177bbf75cf393072
camper0008 <spetakel123@gmail.com>
2022-04-25 10:41:32 +0200

    remove cookiejar

--- docs/api_test/main.ts
+++ docs/api_test/main.ts
@@ -1,14 +1,9 @@
-import { Cookie, CookieJar, wrapFetch } from "./deps.ts";
-
 import { randomUsername } from "./utils.ts";
 
 import { testRegister } from "./tests/register.ts";
 
-const main = () => {
-    const cookieJar = new CookieJar();
-    const fetch = wrapFetch({ cookieJar });
-    const username = randomUsername();
-    testRegister(username, cookieJar);
+const main = async () => {
+    const testUserInfo = await testRegister();
 };
 
 main();
