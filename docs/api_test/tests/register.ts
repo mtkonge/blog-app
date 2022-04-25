ce7dd1ec5a58017ad2bcdd55bcf7659db7baf547
camper0008 <spetakel123@gmail.com>
2022-04-25 10:41:32 +0200

    Tests: Remove cookiejar

--- docs/api_test/tests/register.ts
+++ docs/api_test/tests/register.ts
@@ -1,9 +1,10 @@
-import { CookieJar } from "../deps.ts";
 import { API_URL } from "../api_url.ts";
 import { randomUsername, test } from "../utils.ts";
 
 interface ReturnedRegisterInformation {
     userId: string;
+    username: string;
+    passwordHash: string;
 }
 
 const testMissingPassword = async () => {
@@ -20,12 +21,17 @@ const testMissingPassword = async () => {
         })
     ).json();
 
-    test(res.ok === false, "register w missing password: response was OK");
-    test(
-        res.error === "Invalid username/password",
-        `register w missing password: response error was '${res.error}', expected 'Invalid username/password'`,
-    );
-    test(res.user === null, "register w missing password: user field not null");
+    {
+        test(res.ok === false, "register w missing password: response was OK");
+        test(
+            res.error === "Invalid username/password",
+            `register w missing password: response error was '${res.error}', expected 'Invalid username/password'`,
+        );
+        test(
+            res.user === null,
+            "register w missing password: user field not null",
+        );
+    }
 };
 
 const testMissingUsername = async () => {
@@ -41,46 +47,17 @@ const testMissingUsername = async () => {
         })
     ).json();
 
-    test(res.ok === false, "register w missing username: response was OK");
-    test(
-        res.error === "Invalid username/password",
-        `register w missing username: response error was '${res.error}', expected 'Invalid username/password'`,
-    );
-    test(res.user === null, "register w missing username: user field not null");
-};
-
-const testCorrectRegister = async (
-    username: string,
-): Promise<ReturnedRegisterInformation> => {
-    const body = {
-        username,
-        password: "passwd",
-    } as unknown as BodyInit;
-
-    const res = await (
-        await fetch(API_URL + "/users/register", {
-            method: "POST",
-            body,
-        })
-    ).json();
-
-    test(res.ok === true, "register correctly: response was not OK");
-    test(
-        res.error === null,
-        `register correctly: response error was '${res.error}', expected null`,
-    );
-    test(res.user !== null, "register correctly: user field null");
-    test(res.user?.id !== null, "register correctly: user id null");
-    test(
-        res.user?.username === username,
-        `register correctly: invalid user.username, expected '${username}', got '${res.user.username}'`,
-    );
-    test(
-        res.user?.passwordHash !== null,
-        "register correctly: user password hash null",
-    );
-
-    return { userId: res.user?.password };
+    {
+        test(res.ok === false, "register w missing username: response was OK");
+        test(
+            res.error === "Invalid username/password",
+            `register w missing username: response error was '${res.error}', expected 'Invalid username/password'`,
+        );
+        test(
+            res.user === null,
+            "register w missing username: user field not null",
+        );
+    }
 };
 
 const testDuplicateUsername = async () => {
@@ -104,15 +81,17 @@ const testDuplicateUsername = async () => {
         })
     ).json();
 
-    test(
-        res0.ok === true,
-        "register w duplicate username: first registered user was not OK",
-    );
+    {
+        test(
+            res0.ok === true,
+            "register w duplicate username: first registered user was not OK",
+        );
 
-    test(
-        res1.ok === false,
-        "register w duplicate username: second registered user was OK",
-    );
+        test(
+            res1.ok === false,
+            "register w duplicate username: second registered user was OK",
+        );
+    }
 };
 
 const testUniqueUserId = async () => {
@@ -142,29 +121,67 @@ const testUniqueUserId = async () => {
         })
     ).json();
 
-    test(
-        res0.ok === true,
-        "register unique ids: first registered user was not OK",
-    );
+    {
+        test(
+            res0.ok === true,
+            "register unique ids: first registered user was not OK",
+        );
 
-    test(
-        res1.ok === true,
-        "register unique ids: second registered user was not OK",
-    );
+        test(
+            res1.ok === true,
+            "register unique ids: second registered user was not OK",
+        );
 
-    test(
-        res0.user?.id !== res1.user?.id,
-        "register unique ids: first and second user had the same ids",
-    );
+        test(
+            res0.user?.id !== res1.user?.id,
+            "register unique ids: first and second user had the same ids",
+        );
+    }
 };
 
-export const testRegister = async (
-    username: string,
-    jar: CookieJar,
-): Promise<ReturnedRegisterInformation> => {
+const testCorrectRegister = async (): Promise<ReturnedRegisterInformation> => {
+    const username = randomUsername();
+    const body = {
+        username,
+        password: "passwd",
+    } as unknown as BodyInit;
+
+    const res = await (
+        await fetch(API_URL + "/users/register", {
+            method: "POST",
+            body,
+        })
+    ).json();
+
+    {
+        test(res.ok === true, "register correctly: response was not OK");
+        test(
+            res.error === null,
+            `register correctly: response error was '${res.error}', expected null`,
+        );
+        test(res.user !== null, "register correctly: user field null");
+        test(res.user?.id !== null, "register correctly: user id null");
+        test(
+            res.user?.username === username,
+            `register correctly: invalid user.username, expected '${username}', got '${res.user.username}'`,
+        );
+        test(
+            res.user?.passwordHash !== null,
+            "register correctly: user password hash null",
+        );
+    }
+
+    return {
+        userId: res.user?.id,
+        username,
+        passwordHash: res.user?.passwordHash,
+    };
+};
+
+export const testRegister = async (): Promise<ReturnedRegisterInformation> => {
     await testMissingPassword();
     await testMissingUsername();
     await testDuplicateUsername();
     await testUniqueUserId();
-    return await testCorrectRegister(username);
+    return await testCorrectRegister();
 };
