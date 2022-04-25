981d2a764627a6a2077470595ee1e250b3423c4a
SimonFJ20 <simonfromjakobsen@gmail.com>
2022-04-25 13:31:08 +0200

    Tests: Use blogs create

--- tests/main.ts
+++ tests/main.ts
@@ -3,6 +3,7 @@ import { randomUsername } from "./utils.ts";
 import { testRegister } from "./endpoints/register.ts";
 import { testUsersPublicData } from "./endpoints/users_public_data.ts";
 import { testSessionLogin } from "./endpoints/session_login.ts";
+import { testBlogsCreate } from "./endpoints/blogs_create.ts";
 
 const main = async () => {
     const testuser = {
@@ -12,6 +13,10 @@ const main = async () => {
     const userInfo = await testRegister(testuser);
     const sessionLoginInfo = await testSessionLogin(testuser);
     await testUsersPublicData(userInfo);
+    const blogCreateInfo = await testBlogsCreate({
+        token: sessionLoginInfo.token,
+        userId: userInfo.userId,
+    });
 };
 
 main();
