0fab2703dca5ad994f8dbcdcda127238ce8e68ff
camper0008 <spetakel123@gmail.com>
2022-04-25 13:38:35 +0200

    Tests: bulk incomplete field testing

--- tests/utils.ts
+++ tests/utils.ts
@@ -17,3 +17,53 @@ export const jsonHeaders = () =>
 
 export const jsonAuthHeaders = (token: string) =>
     new Headers({ "Content-Type": "application/json", token: token });
+
+interface ValueFields {
+    [key: string]: any;
+}
+
+export const bulkTestIncompleteValues = async (
+    endpoint: string,
+    headers: Headers,
+    values: ValueFields,
+) => {
+    for (let key in values) {
+        const body = { ...values };
+        body[key] = null;
+        let res = await (
+            await fetch(apiUrl, {
+                headers,
+                body: body as unknown as BodyInit,
+            })
+        ).json();
+
+        test(
+            res.ok === false,
+            `bulk incomplete (null) values with '${endpoint}' failed: response was OK with field '${key}'`,
+        );
+        test(
+            res.error !== "Incomplete",
+            `bulk incomplete (null) values with '${endpoint}' failed: expected error 'Incomplete', was '${res.error}' with field '${key}'`,
+        );
+    }
+
+    for (let key in values) {
+        const body = { ...values };
+        body[key] = undefined;
+        let res = await (
+            await fetch(apiUrl, {
+                headers,
+                body: body as unknown as BodyInit,
+            })
+        ).json();
+
+        test(
+            res.ok === false,
+            `bulk incomplete (undefined) values with '${endpoint}' failed: response was OK with field '${key}'`,
+        );
+        test(
+            res.error !== "Incomplete",
+            `bulk incomplete (undefined) values with '${endpoint}' failed: expected error 'Incomplete', was '${res.error}' with field '${key}'`,
+        );
+    }
+};
