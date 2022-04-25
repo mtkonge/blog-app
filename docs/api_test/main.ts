d8d1ca3e7d0dd46cbaec981868b7616034fc6b9e
SimonFJ20 <simonfromjakobsen@gmail.com>
2022-04-25 10:45:14 +0200

    Tests+Docs: Moved api_test to /tests

--- docs/api_test/main.ts
+++
@@ -1,9 +0,0 @@
-import { randomUsername } from "./utils.ts";
-
-import { testRegister } from "./tests/register.ts";
-
-const main = async () => {
-    const testUserInfo = await testRegister();
-};
-
-main();
--- docs/api_test/utils.ts
+++
@@ -1,9 +0,0 @@
-export const randomUsername = () => {
-    return "test_user_" + Math.floor(Math.random() * 1000);
-};
-
-export const test = (ok: boolean, msg: string) => {
-    if (!ok) {
-        console.log(`test failed: ${msg}`);
-    }
-};
